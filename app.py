73a11ca94d1cbb6e8cc8b4c10dcd12320971d990
shubhamwrejolut <shubham.w@rejolut.com>
2023-11-29 14:00:40 +0530

    feat: working document comparison feature

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ from langchain.agents import initialize_agent, AgentType, Tool
 from langchain.retrievers import ParentDocumentRetriever
 from langchain.storage import InMemoryStore
 from langchain.llms import HuggingFaceHub
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.agents.agent_toolkits import (
     VectorStoreInfo,
     VectorStoreRouterToolkit,
@@ -33,53 +34,58 @@ app = Flask(__name__)
 load_dotenv()
 
 
-@app.route("/")
-def home():
+@app.route("/add-docs")
+def add_documents():
     try:
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.6)
 
-        class DocumentInput(BaseModel):
-            question: str = Field()
-
-        # Create a temporary directory in the script's folder
-        # script_dir = Path(__file__).resolve().parent
-        # temp_dir = os.path.join(script_dir, "tempDir")
-
-        query = "Tell me five differences between cats and dogs"
-        chat = ChatOpenAI(model="gpt-3.5-turbo")
-        repo_id = "google/flan-t5-xxl"
-        llm = HuggingFaceHub(
-            repo_id=repo_id, model_kwargs={"temperature": 0.9, "max_length": 5000}
-        )
         files = [
-            {
-                "name": "cats",
-                "path": "cats.pdf",
-                "description": "use this document to answer questions about  cats",
-            },
-            {
-                "name": "dogs",
-                "path": "dogs.pdf",
-                "description": "use this document to answer questions about  dogs",
-            },
-            {
-                "name": "payment",
-                "path": "payment_security_standards.pdf",
-                "description": "use this document to answer questions about  payments",
-            },
-            {
-                "name": "central_bank_of_kuwait",
-                "path": "central_bank_of_kuwait.pdf",
-                "description": "use this document to answer questions about  central_bank_of_kuwait",
-            },
+            # {
+            #     "name": "cats",
+            #     "path": "cats.pdf",
+            #     "description": "use this document to answer questions about  cats",
+            # },
+            # {
+            #     "name": "dogs",
+            #     "path": "dogs.pdf",
+            #     "description": "use this document to answer questions about  dogs",
+            # },
+            # {
+            #     "name": "MAS",
+            #     "path": "MAS.pdf",
+            #     "description": "use this document to answer questions about technology risk guidelines",
+            # },
+            # {
+            #     "name": "SAMA",
+            #     "path": "SAMA.pdf",
+            #     "description": "use this document to answer questions about  saudi arabia monetary authority",
+            # },
+            # {
+            #     "name": "payment",
+            #     "path": "payment_security_standards.pdf",
+            #     "description": "use this document to answer questions about  payments",
+            # },
+            # {
+            #     "name": "central_bank_of_kuwait",
+            #     "path": "central_bank_of_kuwait.pdf",
+            #     "description": "use this document to answer questions about  central_bank_of_kuwait",
+            # },
         ]
+
+        def directory_loader():
+            loader = PyPDFDirectoryLoader("example_data/")
+            docs = loader.load()
+            return docs
 
         def online_loader():
             # https://arxiv.org/pdf/2302.03803.pdf
             loader = OnlinePDFLoader(
                 "https://www.flame.edu.in/pdfs/fil/presentations/FIL_Stock%20Market.pdf"
             )
-            data = loader.load()
-            return data
+            pages = loader.load()
+            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            docs = text_splitter.split_documents(pages)
+            return docs
 
         def offline_loader(path):
             route = "example_data/" + path
@@ -90,58 +96,10 @@ def home():
             print(docs, "Docs")
             return docs
 
-        def directory_loader():
-            loader = PyPDFDirectoryLoader("example_data/")
-            docs = loader.load()
-            return docs
-
-        def create_vectorstore(file, store):
-            embeddings = create_embeddings()
-            vectorstore = VectorStoreInfo(
-                name=file["name"], description=file["description"], vectorstore=store
-            )
-            return vectorstore
-
         def create_embeddings():
-            embeddings = HuggingFaceEmbeddings()
+            # embeddings = HuggingFaceEmbeddings()
+            embeddings = OpenAIEmbeddings()
             return embeddings
-
-        def create_tools(file, retriever):
-            tool = Tool(
-                args_schema=DocumentInput,
-                name=file["name"],
-                description=f"useful when you want to answer questions about {file['name']}",
-                func=RetrievalQA.from_chain_type(llm=llm, retriever=retriever),
-            )
-            return tool
-
-        def compare_docs(question):
-            vectorstores = []
-            for file in files:
-                docs = offline_loader(file["path"])
-                print("1", file["path"])
-                store = add_docs_to_pinecone(file, docs)
-                print("2")
-                # vectorstore = create_vectorstore(file, store)e
-                vectorstore = create_tools(file, store)
-                print("3")
-                vectorstores.append(vectorstore)
-                print("4")
-
-            print("11")
-            # router_toolkit = VectorStoreRouterToolkit(
-            #     vectorstores=vectorstores, llm=llm
-            # )
-            # agent_executor = create_vectorstore_router_agent(
-            #     llm=llm,
-            #     toolkit=router_toolkit,
-            #     verbose=True,
-            #     handle_parsing_errors="Check your output and make sure it conforms!",
-            # )
-            agent_executor = initialize_agent(tools=vectorstores, llm=llm, verbose=True)
-            # result = agent_executor.run(question)
-            result = agent_executor({"input": f"{question}"})
-            return result
 
         def add_docs_to_pinecone(file, docs):
             embeddings = create_embeddings()
@@ -156,31 +114,124 @@ def home():
                 print(pinecone.list_indexes())
                 pinecone.create_index(name=index_name, metric="cosine", dimension=768)
 
-            vectorDb = Pinecone.from_documents(
-                docs, embeddings, index_name=index_name, namespace=file["name"]
-            )
+            vectorDb = Pinecone.from_documents(docs, embeddings, index_name=index_name)
 
-            # vectorDb = Pinecone.from_existing_index(
-            #     index_name, embeddings, namespace=file["name"]
-            # )
             print(vectorDb, "vectorDb")
             return vectorDb.as_retriever()
 
-        def query_existing_vectors(file, docs):
+        for file in files:
+            docs = offline_loader(file["path"])
+            add_docs_to_pinecone(file, docs)
+            print(file["path"])
+
+        return "Docuemnt added successfully"
+    except Exception as e:
+        raise e
+
+
+@app.route("/")
+def home():
+    try:
+
+        class DocumentInput(BaseModel):
+            question: str = Field()
+
+        query = "Compare cats and dogs in detail"
+        chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+        repo_id = "google/flan-t5-xxl"
+        # llm = HuggingFaceHub(
+        #     repo_id=repo_id, model_kwargs={"temperature": 0.9, "max_length": 5000}
+        # )
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.6)
+        files = [
+            {
+                "name": "cats",
+                "path": "cats.pdf",
+                "description": "use this document to answer questions about  cats",
+            },
+            {
+                "name": "dogs",
+                "path": "dogs.pdf",
+                "description": "use this document to answer questions about  dogs",
+            },
+            {
+                "name": "MAS",
+                "path": "MAS.pdf",
+                "description": "use this document to answer questions about technology risk guidelines",
+            },
+            {
+                "name": "SAMA",
+                "path": "SAMA.pdf",
+                "description": "use this document to answer questions about  saudi arabia monetary authority",
+            },
+            {
+                "name": "payment",
+                "path": "payment_security_standards.pdf",
+                "description": "use this document to answer questions about  payments",
+            },
+            {
+                "name": "central_bank_of_kuwait",
+                "path": "central_bank_of_kuwait.pdf",
+                "description": "use this document to answer questions about  central_bank_of_kuwait",
+            },
+        ]
+
+        def create_vectorstore(file, store):
             embeddings = create_embeddings()
+            vectorstore = VectorStoreInfo(
+                name=file["name"], description=file["description"], vectorstore=store
+            )
+            return vectorstore
+
+        def create_embeddings():
+            # embeddings = HuggingFaceEmbeddings()
+            embeddings = OpenAIEmbeddings()
+            return embeddings
+
+        def create_tools(file, retriever):
+            tool = Tool(
+                args_schema=DocumentInput,
+                name=file["name"],
+                description=f"useful when you want to answer questions about {file['name']}",
+                func=RetrievalQA.from_chain_type(
+                    llm=llm,
+                    retriever=retriever,
+                ),
+            )
+            return tool
+
+        def compare_docs(question):
+            vectorstores = []
+            for file in files:
+                store = add_docs_to_pinecone()
+                vectorstore = create_tools(file, store)
+                vectorstores.append(vectorstore)
+
+            agent_executor = initialize_agent(
+                tools=vectorstores,
+                llm=llm,
+                verbose=True,
+                agent=AgentType.OPENAI_FUNCTIONS,
+            )
+
+            result = agent_executor({"input": f"{question}"})
+            return result
+
+        def add_docs_to_pinecone():
+            embeddings = create_embeddings()
+
             pinecone.init(
                 api_key=os.getenv("PINECONE_API_KEY"),
                 environment=os.getenv("PINECONE_ENV"),
             )
             index_name = os.getenv("PINECONE_INDEX_NAME")
-            print(pinecone.list_indexes())
-            docsearch = Pinecone.from_existing_index(index_name, embeddings)
-            print(docsearch, "DocSearch")
-            # query = "Wha is the management of Information Access"
-            # result = docsearch.similarity_search(question)
-            # print(result[0].page_content)
 
-            return docsearch
+            vectorDb = Pinecone.from_existing_index(
+                index_name,
+                embeddings,
+            )
+            print(vectorDb, "vectorDb")
+            return vectorDb.as_retriever()
 
         response = compare_docs(query)
         # response = query_existing_vectors(query)
